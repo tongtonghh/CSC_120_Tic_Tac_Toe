a18628a828a1acd4f4adbc9ba45c75d87d489feb
黄彤 <huangtong@huangtongdembp.lan>
2022-04-25 08:06:09 -0400

    Board Printing functionality added

---
+++ board.py
@@ -0,0 +1,10 @@
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-",]
+
+def display_board():
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[6] + "|" + board[7] + "|" + board[8])
+
+display_board()
